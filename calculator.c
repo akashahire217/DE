1a739d722cf18d7c6de5e3d15fa6ca31ba76d895
akashahire217 <127412019+akashahire217@users.noreply.github.com>
2023-03-13 20:15:35 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,26 @@
+#include "calculator.h"
+
+int main()
+{
+	int a,b;
+	a=20;
+	b=50;
+	addition (a,b);
+
+	int p,q;
+	p=10;
+	q=20;
+	substraction (p,q);
+
+
+	int s,k;
+	s=4;
+	k=12;
+	multiplication (s,k);
+
+
+	int m,d;
+	m=20;
+	d=2;
+	division(m,d);
+}
---
+++ calculator.h
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+     void addition(int x, int y)
+     {
+      printf("%d\n", x+y);
+     }
+     void substraction(int x,int y)
+     {
+         printf("%d\n",x-y);
+     }
+     void multiplication(int x,int y)
+     {
+         printf("%d\n",x*y);
+     }
+     void division(int x,int y)
+     {
+         printf("%d\n",x/y);
+     }
