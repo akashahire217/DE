088a48492708dc93663d9fff695fa178db06060e
akashahire217 <127412019+akashahire217@users.noreply.github.com>
2023-03-13 23:11:04 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include "addition.h" 
+
+
+int main(){
+    int first_value, second_value;
+    first_value = 55;
+    second_value = 15;
+    addition(first_value, second_value);
+    addition(40, 10);
+}
---
+++ addition.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void addition(int a, int b){
+    printf("%d\n", a+b);
+}
